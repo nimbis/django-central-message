aff3ca2b625ec00d67517df41d5057687b603757
Shaun Brady <shaun.brady@nimbisservices.com>
2014-05-09 02:20:03 -0400

    Simple test.  Create messages, count them, delete CentralMessage, make sure they all leave

--- tests/tests.py
+++ tests/tests.py
@@ -1,54 +1,56 @@
 import os
-from django.test import TestCase
+import datetime
+from pytz import UTC
 
-# class SimpleTestCase(TestCase):
-#    '''
-#    Simple test cases that verifies basic model functionality.
-#    '''
-#
-#    def setUp(self):
-#        '''
-#        Set up the test environment.
-#        '''
-#
-#        # enable debug mode
-#        os.environ["DEBUG"] = "True"
-#
-#    def tearDown(self):
-#        '''
-#        Tear down the test environment.
-#        '''
-#
-#        pass
-#
-#    def test_crud(self):
-#        '''
-#        Create, store, update, and delete a rich product.
-#        '''
-#
-#        # create a product
-#        from shop_richproduct.models import RichProduct
-#        product = RichProduct()
-#        product.name = "Test Product"
-#        product.slug = "test-product"
-#        product.description = "this is a test product"
-#        product.unit_price = 6.50
-#        product.active = True
-#
-#        # save it
-#        product.save()
-#        product = None
-#
-#        # retrieve it
-#        product = RichProduct.objects.get(slug="test-product")
-#
-#        # verify it's properties
-#        self.assertEquals(product.name, "Test Product")
-#        self.assertEquals(product.slug, "test-product")
-#        self.assertEquals(product.description, "this is a test product")
-#        self.assertEquals(product.unit_price, 6.50)
-#        self.assertEquals(product.active, True)
-#
-#        # delete it
-#        product.delete()
-#        product = None
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+import messages_extends
+from central_message.models import CentralMessage
+
+
+class SimpleTestCase(TestCase):
+    '''
+    Simple test cases that verifies basic model functionality.
+    '''
+
+    def setUp(self):
+        '''
+        Set up the test environment.
+        '''
+
+        # enable debug mode
+        os.environ["DEBUG"] = "True"
+        self.user1 = User.objects.create(username="test1")
+        self.user2 = User.objects.create(username="test2")
+        self.user3 = User.objects.create(username="test3")
+
+    def tearDown(self):
+        '''
+        Tear down the test environment.
+        '''
+
+        pass
+
+    def test_messages(self):
+        """
+        Testing that .generate() creates the same number of Messages as Users
+        """
+
+        central_message = CentralMessage.objects.create(
+            user=self.user1,
+            message="Test Message",
+            level=messages_extends.SUCCESS_PERSISTENT,
+            extra_tags="",
+            expires=datetime.datetime.now(tz=UTC))
+
+        central_message.generate()
+
+        num_users = User.objects.all()
+        num_messes = messages_extends.models.Message.objects.all()
+        self.assertEqual(len(num_users), len(num_messes))
+
+        # Killing Central Message, Messages should follow
+        central_message.delete()
+        num_messes = messages_extends.models.Message.objects.all()
+        self.assertEqual(0, len(num_messes))
