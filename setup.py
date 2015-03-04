66f9b824d00cc3c3ba9d574a2b24b486f09d0dfe
Eric Amador <eric.amador14@gmail.com>
2015-03-04 11:44:56 -0500

    Updating setup.py

--- setup.py
+++ setup.py
@@ -6,16 +6,15 @@ setup(
     version="0.1.2",
     author="Nimbis Services, Inc.",
     author_email="info@nimbisservices.com",
-    description="Wrapper for extends-messages to send to multiple users (all "
-    "for now) from the admin page..",
+    description="Wrapper for extends-messages to send to multiple "
+                "users (all for now) from the admin page..",
     license="BSD",
     packages=find_packages(exclude=["tests", ]),
     install_requires=[
         'Django',
-        'pytz',
-        'django-messages-extends',
+        'pytz >= 2014.2',
+        'django-messages-extends >= 0.2',
     ],
     zip_safe=False,
     include_package_data=True,
-    test_suite="tests.main",
 )
